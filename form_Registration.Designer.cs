306dee16a734b3773b0317c985fff69553472134
Thanh <thanhtran@student.purdueglobal.edu>
2023-01-09 21:58:53 -0800

    finished creating user registration form and functionality

--- form_Registration.Designer.cs
+++ form_Registration.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.btn_RegistrationBack = new System.Windows.Forms.Button();
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.lbl_UserPassword = new System.Windows.Forms.Label();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_UserPassword = new System.Windows.Forms.TextBox();
+            this.btn_CreateUser = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_RegistrationBack
@@ -42,21 +47,77 @@
             this.btn_RegistrationBack.UseVisualStyleBackColor = true;
             this.btn_RegistrationBack.Click += new System.EventHandler(this.btn_RegistrationBack_Click);
             // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserName.Location = new System.Drawing.Point(233, 148);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(148, 39);
+            this.lbl_UserName.TabIndex = 1;
+            this.lbl_UserName.Text = "Username:";
+            // 
+            // lbl_UserPassword
+            // 
+            this.lbl_UserPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserPassword.Location = new System.Drawing.Point(233, 312);
+            this.lbl_UserPassword.Name = "lbl_UserPassword";
+            this.lbl_UserPassword.Size = new System.Drawing.Size(148, 39);
+            this.lbl_UserPassword.TabIndex = 1;
+            this.lbl_UserPassword.Text = "Password:";
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_UserName.Location = new System.Drawing.Point(437, 152);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(302, 35);
+            this.txt_UserName.TabIndex = 2;
+            // 
+            // txt_UserPassword
+            // 
+            this.txt_UserPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_UserPassword.Location = new System.Drawing.Point(437, 309);
+            this.txt_UserPassword.Name = "txt_UserPassword";
+            this.txt_UserPassword.Size = new System.Drawing.Size(302, 35);
+            this.txt_UserPassword.TabIndex = 2;
+            // 
+            // btn_CreateUser
+            // 
+            this.btn_CreateUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CreateUser.Location = new System.Drawing.Point(207, 511);
+            this.btn_CreateUser.Name = "btn_CreateUser";
+            this.btn_CreateUser.Size = new System.Drawing.Size(219, 85);
+            this.btn_CreateUser.TabIndex = 0;
+            this.btn_CreateUser.Text = "Create";
+            this.btn_CreateUser.UseVisualStyleBackColor = true;
+            this.btn_CreateUser.Click += new System.EventHandler(this.btn_CreateUser_Click);
+            // 
             // form_Registration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.txt_UserPassword);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.lbl_UserPassword);
+            this.Controls.Add(this.lbl_UserName);
+            this.Controls.Add(this.btn_CreateUser);
             this.Controls.Add(this.btn_RegistrationBack);
             this.Name = "form_Registration";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "form_Registration";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_RegistrationBack;
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label lbl_UserPassword;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_UserPassword;
+        private System.Windows.Forms.Button btn_CreateUser;
     }
 }
--- form_Registration.cs
+++ form_Registration.cs
@@ -17,11 +17,50 @@ namespace Astect
             InitializeComponent();
         }
 
+        Database db = new Database();
         private void btn_RegistrationBack_Click(object sender, EventArgs e)
         {
             form_LogIn formLogIn = new form_LogIn();
             formLogIn.Show();
             this.Hide();
+        }
+
+        private void btn_CreateUser_Click(object sender, EventArgs e)
+        {
+            List<String> usernameList = new List<String>();
+            usernameList = db.getUserNames();
+            Boolean isUserNameTaken = true;
+
+            if (txt_UserName.Text != "" && txt_UserPassword.Text != "")
+            {
+                foreach (String per in usernameList)
+                {
+                    if (txt_UserName.Text == per)
+                    {
+                        isUserNameTaken = true;
+                    }
+                    else
+                    {
+                        isUserNameTaken = false;
+                    }
+                }
+                if (isUserNameTaken == false)
+                {
+                    db.createNewUser(txt_UserName.Text, txt_UserPassword.Text);
+                    MessageBox.Show("Account created, please log in");
+                    form_LogIn formLogIn = new form_LogIn();
+                    formLogIn.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username is taken");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a user name and password");
+            }
         }
     }
 }
